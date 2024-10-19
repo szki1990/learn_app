17cc4a0e64b58ca05aa9737de63df176c626b70f
RikutoSuzuki <163372203+szki1990@users.noreply.github.com>
2024-10-19 11:45:08 +0000

    練習

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'top' => 'homes#top'
+  get 'books/new'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show', as: 'book'
+  post 'books' => 'books#create'
+  get '/top' => 'homes#top'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
 end
