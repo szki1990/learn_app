ad289963910ac65e6f65c2b3609c9d664afaf764
RikutoSuzuki <163372203+szki1990@users.noreply.github.com>
2024-10-23 11:05:16 +0000

    bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'homes#top'
   get 'books/new'
   get 'books' => 'books#index'
   get 'books/:id' => 'books#show', as: 'book'
   post 'books' => 'books#create'
   get '/top' => 'homes#top'
+  get '/homes/about' => 'homes#about'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
 end
