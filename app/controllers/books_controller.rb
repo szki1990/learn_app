b9e91a2b5d2d4502ebf0f18e5ebbb1c97a224ca2
RikutoSuzuki <163372203+szki1990@users.noreply.github.com>
2024-10-23 11:37:49 +0000

    destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,36 +1,42 @@
 class BooksController < ApplicationController
     
-    def new
-        @book = Book.new
-    end 
+  def new
+    @book = Book.new
+  end 
     
-    def index
-        @books = Book.all
-    end 
+  def index
+    @books = Book.all
+  end 
     
-    def create
-        book = Book.new(book_params)
-        book.save
-        redirect_to book_path(book.id)
-    end 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end 
     
-    def edit
-        @book = Book.find(params[:id])
-    end 
+  def edit
+    @book = Book.find(params[:id])
+  end 
     
-    def update
-        book = Book.find(params[:id])
-        book.update(book_params)
-        redirect_to book_path(book.id)
-    end 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end 
     
-    def show
-        @book = Book.find(params[:id])
-    end 
+  def show
+    @book = Book.find(params[:id])
+  end 
     
-    private
+  def destroy
+    @book = Book.find(params[:id])
+    @book.delete
+    redirect_to books_path(@book.id)
+  end 
     
-    def book_params
-        params.require(:book).permit(:title, :body)
-    end 
+  private
+    
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'books/new'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'book'
-  post 'books' => 'books#create'
+  resources :books, only: [:new, :index, :show, :create, :edit, :update, :destroy]
   get '/top' => 'homes#top'
   get '/homes/about' => 'homes#about'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
+  
 end
